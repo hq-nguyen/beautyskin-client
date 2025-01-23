f94ad7da38892be94cfc97290596a96dd855cc62
hq-nguyen <quannvhqe180068@fpt.edu.vn>
2025-01-23 14:11:34 +0700

    add register page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../components/Banner/Banner'
+import PopularClassification from '../components/PopularClassification/PopularClassification'
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <PopularClassification />
       
     </div>
   )
