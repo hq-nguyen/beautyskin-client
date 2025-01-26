5e3eae70ac974772f2d5b53eb9715aba677c6dcb
hq-nguyen <quannvhqe180068@fpt.edu.vn>
2025-01-26 11:37:38 +0700

    responsive navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,30 +8,30 @@ const Navbar = () => {
   const [visible, setVisible] = useState(false);
 
   return (
-    <div className='navbar flex items-center justify-between py-5 font-medium px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+    <div className='navbar flex items-center justify-between py-5 font-medium px-4 lg:px-[9vw]'>
 
       <Link to='/'>
         <img src={assets.logo} className='w-36' alt="" />
 
       </Link>
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
-        <NavLink to='/about' className='flex flex-col items-center gap-1'>
+        <NavLink to='/about' className='navbar-link flex flex-col items-center gap-1'>
           <p>Giới thiệu</p>
           <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
         </NavLink>
-        <NavLink to='/shop' className='flex flex-col items-center gap-1'>
+        <NavLink to='/shop' className='navbar-link flex flex-col items-center gap-1'>
           <p>Mua hàng</p>
           <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
         </NavLink>
-        <NavLink to='/blog' className='flex flex-col items-center gap-1'>
+        <NavLink to='/blog' className='navbar-link flex flex-col items-center gap-1'>
           <p>Blog</p>
           <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
         </NavLink>
-        <NavLink to='/news' className='flex flex-col items-center gap-1'>
+        <NavLink to='/news' className='navbar-link flex flex-col items-center gap-1'>
           <p>Tin tức</p>
           <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
         </NavLink>
-        <NavLink to='/test' className='flex flex-col items-center gap-1'>
+        <NavLink to='/test' className='navbar-link flex flex-col items-center gap-1'>
           <p>Trắc nghiệm loại da</p>
           <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
         </NavLink>
@@ -39,15 +39,15 @@ const Navbar = () => {
       </ul>
 
       <div className='flex items-center gap-6'>
+        <img src={assets.wishlist_icon} className='w-5 cursor-pointer' alt="" />
         <img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
 
         <div className='group relative'>
           <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
           <div className='absolute dropdown-menu hidden right-0 pt-4 group-hover:block'>
-            <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
-              <p className='cursor-pointer hover:text-black'>My Profile</p>
-              <p className='cursor-pointer hover:text-black'>Orders</p>
-              <p className='cursor-pointer hover:text-black'>Logout</p>
+            <div className='dropdown-menu-box flex flex-col gap-2 w-36 py-3 px-5 bg-white text-gray-500 rounded'>
+              <Link to='/login' className='login-section cursor-pointer hover:text-white'>Đăng nhập</Link>
+              <Link to='/register' className='register-section cursor-pointer hover:text-white'>Đăng ký</Link>
             </div>
           </div>
         </div>
@@ -60,7 +60,7 @@ const Navbar = () => {
       </div>
 
       {/* Sidebar menu for small screens */}
-      <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
+      <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-60' : 'w-0'} z-50`}>
         <div className='flex flex-col text-gray-600'>
           <div onClick={() => setVisible(false)} className='flex items-centers gap-4 p-3 cursor-pointer'>
             <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
