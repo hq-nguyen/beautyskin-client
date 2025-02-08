37fa72c813f74cba6d8f8a5cf6bee7bcfa01a8b6
Hung <truongquochung0704@gmail.com>
2025-02-08 22:11:28 +0700

    add deploy.sh

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useState, useEffect } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
