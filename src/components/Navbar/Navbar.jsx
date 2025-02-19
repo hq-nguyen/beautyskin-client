e1881ad999fc96fd06b93f8521516a6804e59895
hq-nguyen <quannvhqe180068@fpt.edu.vn>
2025-02-19 21:00:44 +0700

    fix: fix sticky feature of navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,30 +8,30 @@ const Navbar = () => {
   const [visible, setVisible] = useState(false);
 
   return (
-    <div>
-      <div className='navbar sticky top-0 z-40 flex items-center justify-between py-5 font-medium px-4 lg:px-[9vw]'>
+    <div className='navbar sticky top-0 z-50'>
+      <div className='navbar sticky top-0  flex items-center justify-between py-5 font-medium px-4 lg:px-[9vw]'>
         <Link to='/'>
           <img src={assets.logo} className='w-36' alt="" />
 
         </Link>
         <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
-          <NavLink to='/about' className='navbar-link flex flex-col items-center gap-1'>
+          <NavLink to='/about' className='hover:text-rose-600 duration-300 flex flex-col items-center gap-1'>
             <p>Giới thiệu</p>
             <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
           </NavLink>
-          <NavLink to='/shop' className='navbar-link flex flex-col items-center gap-1'>
+          <NavLink to='/shop' className='hover:text-rose-600 duration-300 flex flex-col items-center gap-1'>
             <p>Mua hàng</p>
             <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
           </NavLink>
-          <NavLink to='/blog' className='navbar-link flex flex-col items-center gap-1'>
+          <NavLink to='/blog' className='hover:text-rose-600 duration-300 flex flex-col items-center gap-1'>
             <p>Blog</p>
             <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
           </NavLink>
-          <NavLink to='/news' className='navbar-link flex flex-col items-center gap-1'>
+          <NavLink to='/news' className='hover:text-rose-600 duration-300 flex flex-col items-center gap-1'>
             <p>Tin tức</p>
             <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
           </NavLink>
-          <NavLink to='/test' className='navbar-link flex flex-col items-center gap-1'>
+          <NavLink to='/test' className='hover:text-rose-600 duration-300 flex flex-col items-center gap-1'>
             <p>Trắc nghiệm loại da</p>
             <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
           </NavLink>
@@ -44,7 +44,7 @@ const Navbar = () => {
 
           <div className='group relative'>
             <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
-            <div className='absolute dropdown-menu hidden right-0 pt-4 group-hover:block'>
+            <div className='absolute z-100 hidden right-0 pt-4 group-hover:block'>
               <div className='dropdown-menu-box flex flex-col gap-2 w-36 py-3 px-5 bg-white text-gray-500 rounded'>
                 <Link to='/login' className='login-section cursor-pointer hover:text-white'>Đăng nhập</Link>
                 <Link to='/register' className='register-section cursor-pointer hover:text-white'>Đăng ký</Link>
@@ -58,7 +58,6 @@ const Navbar = () => {
 
           <img onClick={() => setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
         </div>
-
       </div>
 
       {/* Sidebar menu for small screens */}
@@ -82,8 +81,6 @@ const Navbar = () => {
         </div>
       </div>
     </div>
-
-
   )
 }
 
