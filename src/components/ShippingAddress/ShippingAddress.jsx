aab7124032ad5681c6f2d1122b9dd57d62b74ead
Hung <truongquochung0704@gmail.com>
2025-02-20 10:07:21 +0700

    add update default address page

--- src/components/ShippingAddress/ShippingAddress.jsx
+++ src/components/ShippingAddress/ShippingAddress.jsx
@@ -31,7 +31,7 @@ const ShippingAddress = () => {
         <div className="flex-1 bg-white p-6 rounded-lg mt-[35px] shadow-[0px_0px_10px_rgba(0,0,0,0.1)]">
             <div className="flex justify-between items-center mb-8">
                 <h2 className="text-2xl font-bold">Số địa chỉ nhận hàng</h2>
-                <button className="bg-[#d90429] text-white px-4 py-2 rounded-full flex items-center gap-2 hover:bg-gray-700 transition-colors">
+                <button className="bg-gray-800 text-white px-4 py-2 rounded-full flex items-center gap-2 hover:bg-gray-700 transition-colors">
                     <Link to={'/user/add-address'}>Thêm địa chỉ mới →</Link>
                 </button>
             </div>
