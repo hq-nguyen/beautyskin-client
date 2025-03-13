de31671cc238ce2dff9fada1fd11bd866d1aa002
hq-nguyen <quannvhqe180068@fpt.edu.vn>
2025-03-13 15:16:34 +0700

    fix: fix header login by role

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
 
   const handleSearch = async (e) => {
     e.preventDefault();
-    if (!searchQuery.trim()) return; 
+    if (!searchQuery.trim()) return;
     try {
       //Lấy các sản phẩm
       const allProducts = await fetchProducts();
@@ -91,8 +91,8 @@ const Navbar = () => {
                 onChange={(e) => setSearchQuery(e.target.value)}
               />
               {searchQuery && (
-                <button 
-                  type="button" 
+                <button
+                  type="button"
                   onClick={clearSearch}
                   className="text-gray-400 hover:text-gray-600 mr-1"
                 >
@@ -100,8 +100,8 @@ const Navbar = () => {
                 </button>
               )}
               <div className="h-6 w-px bg-gray-300 mx-2"></div>
-              <button 
-                type="submit" 
+              <button
+                type="submit"
                 className="rounded-full p-1"
               >
                 <IoSearchOutline size={20} className="text-gray-500" />
@@ -116,21 +116,44 @@ const Navbar = () => {
             <div className='group relative'>
               <CiUser className='w-[1.6rem] h-[2rem] cursor-pointer' />
               <div className='absolute z-100 hidden right-0 pt-4 group-hover:block'>
-                <div className='dropdown-menu-box flex flex-col gap-1 w-48 py-2 px-5 bg-white text-gray-500 rounded shadow-lg'>
-                  <div>
-                    <Link to='/user' className='block cursor-pointer hover:text-rose-600 duration-300'>
-                      Thông tin tài khoản
-                    </Link>
-                  </div>  
-                  <div>
-                    <button 
-                      onClick={handleLogout} 
-                      className='w-full text-left cursor-pointer hover:text-rose-600 duration-300'
-                    >
-                      Đăng xuất
-                    </button>
+                {user.roleEnum === "MANAGER" ? (
+                  <div className='dropdown-menu-box flex flex-col gap-1 w-36 py-2 px-2 bg-white text-gray-500 rounded shadow-lg'>
+                    <>
+                      <div>
+                        <Link to='/admin' className='block cursor-pointer hover:text-white py-1 px-2 hover:bg-rose-600 rounded-sm duration-300'>
+                          Dashboard
+                        </Link>
+                      </div>
+                      <div>
+                        <button
+                          onClick={handleLogout}
+                          className='w-full text-left cursor-pointer hover:text-white py-1 px-2 hover:bg-rose-600 rounded-sm duration-300'
+                        >
+                          Đăng xuất
+                        </button>
+                      </div>
+                    </>
                   </div>
-                </div>
+
+                ) : (
+                  <div className='dropdown-menu-box flex flex-col gap-1 w-48 py-2 px-2 bg-white text-gray-500 rounded shadow-lg'>
+                    <>
+                      <div>
+                        <Link to='/user' className='block cursor-pointer hover:text-white hover:bg-rose-600 px-2 py-1 rounded-sm duration-300'>
+                          Thông tin tài khoản
+                        </Link>
+                      </div>
+                      <div>
+                        <button
+                          onClick={handleLogout}
+                          className='w-full text-left cursor-pointer hover:text-white hover:bg-rose-600 px-2 py-1 rounded-sm duration-300'
+                        >
+                          Đăng xuất
+                        </button>
+                      </div>
+                    </>
+                  </div>
+                )}
               </div>
             </div>
           ) : (
@@ -145,10 +168,10 @@ const Navbar = () => {
             </div>
           )}
           <Link to='/checkout/cart' className='relative'>
-            <img 
-              src={assets.cart_icon} 
-              className='w-5 min-w-5' 
-              alt="" 
+            <img
+              src={assets.cart_icon}
+              className='w-5 min-w-5'
+              alt=""
             />
             <p className='absolute right-[-8px] top-[-5px] w-4 text-center leading-4 bg-red-600 text-white aspect-square rounded-full text-[8px]'>
               {totalQuantity}
@@ -174,6 +197,9 @@ const Navbar = () => {
           <NavLink onClick={() => setVisible(false)} className='text-primary py-2 pl-5 border' to='/shop'>Mua hàng</NavLink>
           <NavLink onClick={() => setVisible(false)} className='text-primary py-2 pl-5 border' to='/about'>Về chúng tôi</NavLink>
           <NavLink onClick={() => setVisible(false)} className='text-primary py-2 pl-5 border' to='/contact'>Liên hệ</NavLink>
+          {user && user.roleEnum === "MANAGER" && (
+            <NavLink onClick={() => setVisible(false)} className='text-primary py-2 pl-5 border' to='/dashboard'>Dashboard</NavLink>
+          )}
         </div>
       </div>
     </div>
