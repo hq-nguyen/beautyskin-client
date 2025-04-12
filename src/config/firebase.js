6903b3afa6c0fb92c9faa70e90d08595e30cbeb1
hq-nguyen <nguyenhaiquan.data@gmail.com>
2025-04-12 07:51:08 +0700

    add .env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,13 +3,13 @@ import { getStorage } from "firebase/storage";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqn_Hx8zN3ugbj5i45V4igcu3Edh2notY",
-  authDomain: "beaty-skin-f196a.firebaseapp.com",
-  projectId: "beaty-skin-f196a",
-  storageBucket: "beaty-skin-f196a.firebasestorage.app",
-  messagingSenderId: "278641367106",
-  appId: "1:278641367106:web:6fda8b46c51d88271035db",
-  measurementId: "G-7TVY9R9LLF"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const storageApp = initializeApp(firebaseConfig, "storage");
